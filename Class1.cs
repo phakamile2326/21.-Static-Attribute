381895af5d913c2954cf2a457639403caa10ceeb
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-10 13:31:35 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Static_Attribute
+{
+    internal class Program
+
+    {
+        static void Main(string[] args) 
+        {
+            Songs holiday = new Songs("Holiday", "green day", 2);
+            Songs preach = new Songs("preach","John Legend", 5);
+            Console.WriteLine(Songs.songCount);
+            Console.ReadLine();        
+        
+        
+        } 
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Static_Attribute
+{
+    internal class Songs
+    {
+        public string title;
+        public string artist;
+        public int duration;
+
+        public static int songCount = 0;//declaration
+        public Songs (string aTitle, string aArtist, int aDuration)
+        {
+            this.title = aTitle;
+            this.artist = aArtist;
+            this.duration = aDuration;
+            songCount++;//increment each time a song is added
+        }
+    }
+}
